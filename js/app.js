a363b0e80048b1342249cc19fd1cc3dffbee2712
Eroshevsky Alexander <a.eroshevsky@gmail.com>
2015-01-27 11:47:41 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+    $('#showMenu').click(function () {
+            $('.perspective').css('background','rgba(0,0,0, 0.5)');
+    });
+    $('.container').click(function () {
+            $('.perspective').css('background','');
+    });
+
+});
